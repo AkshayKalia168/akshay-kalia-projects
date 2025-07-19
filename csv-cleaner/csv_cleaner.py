4b5f1d1528abb09b18fb907c9aadc5c3c60f2059
AkshayKalia168 <akshaykalia231738@gmail.com>
2025-07-19 15:39:06 -0700

    Update csv_cleaner.py

--- csv-cleaner/csv_cleaner.py
+++ csv-cleaner/csv_cleaner.py
@@ -2,7 +2,7 @@
 import csv
 import sys
 def main():
-    lists = []
+    records = []
     if len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
     elif len(sys.argv) < 3:
@@ -14,12 +14,12 @@ def main():
             reader = csv.DictReader(file)
             for row in reader:
                 last, first = row["name"].split(", ")
-                house = row["house"]
-                lists.append({"first": first, "last": last, "house": house})
+                department = row["department"]
+                records.append({"first": first, "last": last, "department": department})
     except(FileNotFoundError):
         sys.exit(f"Could not read {inputfile}")
     with open(f"{outputfile}", "w", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=["first", "last", "house"])
+        writer = csv.DictWriter(file, fieldnames=["first", "last", "department"])
         writer.writeheader()
         for row in lists:
             writer.writerow(row)
